88943a63581c813183c56d65480b3302ea511058
Alexey Stepankov <xeurun@yahoo.com>
2018-09-22 22:13:58 +0400

    Some fix

--- .docker/nginx.Dockerfile
+++ .docker/nginx.Dockerfile
@@ -1,7 +1,9 @@
 FROM nginx:alpine
 
+RUN rm /etc/nginx/conf.d/default.conf
 COPY /etc/nginx/nginx.conf /etc/nginx/
-COPY /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/
+COPY /etc/nginx/conf.d/dev.conf /etc/nginx/conf.d/
+COPY /etc/nginx/conf.d/prod.conf /etc/nginx/conf.d/
 
 RUN ln -snf /usr/share/zoneinfo/${TZ} /etc/localtime && echo ${TZ} > /etc/timezone
 
---
+++ src/Controller/MainController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function main()
+    {
+        return new Response();
+    }
+}
